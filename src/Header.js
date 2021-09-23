0482cdfbb4e6a1fef3f432bee8fe031db40955e0
Armando <a.guerra58@hotmail.com>
2021-09-23 19:18:36 -0300

    a

---
+++ src/Header.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+import "./styles/navigationBar.css";
+
+class Header extends React.Component {
+  render() {
+    return (
+      <nav className="navigationBar">
+        <img src="https://media-exp3.licdn.com/dms/image/C4D0BAQFalja6B0Vl8A/company-logo_200_200/0/1625490679503?e=2159024400&v=beta&t=nqTaSKJaQJJZIhzEbWFriLCfSlgMlIxslVeGFdU54_o" alt="logotrybe" id="logo-trybe"/>
+        <Link className="linkpath" to="/">Home</Link>
+        <Link className="linkpath" to="/about">About</Link>
+        <Link className="linkpath" to="/projects">Projects</Link>
+      </nav>
+    );
+  }
+}
+
+export default Header;
