e84c38394a4e36b7ee27c70c1549009d6114a717
Armando <a.guerra58@hotmail.com>
2021-09-23 19:24:31 -0300

    a

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ function App() {
           </nav>
           <Switch>
             <Route exact path="/portfolio" component={Home} />
+            <Route exact path="/" component={ Home } />
             <Route path="/about" component={About} />
             <Route path="/projects" component={Projects} />
           </Switch>
