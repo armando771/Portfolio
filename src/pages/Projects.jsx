cb600caa5590d675b58d0c0f24b3efc5cf0bfdf7
Armando <a.guerra58@hotmail.com>
2021-09-23 23:34:54 -0300

    new project

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -12,11 +12,12 @@ function Projects(){
         <div id="links-container">
         <a href="https://armando771.github.io/Color-Palet.github.io/" target="_blank" rel="noreferrer" >Project: Pixel Art</a>
         <a href="https://armando771.github.io/Project-Shopping-Card/" target="_blank" rel="noreferrer">Project: Shopping Cart</a>
+        <a href="https://armando771.github.io/Project-Todo-List/" target="_blank" rel="noreferrer">Project: Todo List</a>
         <a href="https://github.com/tryber/sd-012-project-zoo-functions/blob/Armando-Guerra-zoo-functions-project/src/zoo.js" target="_blank" rel="noreferrer">Project: Zoo Functions</a>
         <a href="https://github.com/tryber/sd-012-project-jest/tree/armando-guerra-project-jest/test" target="_blank" rel="noreferrer">Project: Test Jest</a>
         <a href="https://github.com/tryber/sd-012-project-react-testing-library/tree/Armando-Guerra-Project-React-Testing-Library/src/tests" target="_blank" rel="noreferrer">Project: Test React Library</a>
         <a href="https://github.com/tryber/sd-12-project-frontend-online-store/tree/main-group-7/src/components" target="_blank" rel="noreferrer">Project: Front End Online Store</a>
-        <a href="https://armando771.github.io/Project-Todo-List/" target="_blank" rel="noreferrer">Project: Todo List</a>
+        <a href="https://armando771.github.io/ProjectMovieCrud/" target="_blank" rel="noreferrer">Project:Movie Library Crud</a>
         </div>
       </div>
     );
