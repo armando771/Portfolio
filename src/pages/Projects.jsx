7b2f391a9318a103dcfc69edc3f96e9b6b5fa09e
Armando <a.guerra58@hotmail.com>
2021-09-23 21:40:23 -0300

    inserindo projetos

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -2,8 +2,7 @@ import React from 'react';
 
 import '../styles/projects.css'
 
-class Projects extends React.Component {
-  render() {
+function Projects(){
     return (
       <div id="main-container">
         <div id="informations-container">
@@ -11,17 +10,16 @@ class Projects extends React.Component {
         <p id="paragraph"> Aqui esta um pouco sobre o que ja fiz nesta caminhada como estudante de desenvolvimento </p>
         </div>
         <div id="links-container">
-        <a href="https://armando771.github.io/Color-Palet.github.io/" target="_blank">Project: Pixel Art</a>
-        <a href="https://github.com/tryber/sd-012-project-zoo-functions/blob/Armando-Guerra-zoo-functions-project/src/zoo.js" target="_blank">Project: Zoo Functions</a>
-        <a href="https://github.com/tryber/sd-012-project-shopping-cart/blob/armando-guerra-shopping-cart-project/script.js" target="_blank">Project: Shopping Cart</a>
-        <a href="https://github.com/tryber/sd-012-project-jest/tree/armando-guerra-project-jest/test" target="_blank">Project: Test Jest</a>
-        <a href="https://github.com/tryber/sd-012-project-react-testing-library/tree/Armando-Guerra-Project-React-Testing-Library/src/tests" target="_blank">Project: Test React Library</a>
-        <a href="https://github.com/tryber/sd-12-project-frontend-online-store/tree/main-group-7/src/components" target="_blank">Project: Front End Online Store</a>
-        <a href="https://github.com/tryber/sd-012-project-todo-list/blob/armando-guerra-project-todo-list/script.js" target="_blank">Project: Todo List</a>
+        <a href="https://armando771.github.io/Color-Palet.github.io/" target="_blank" rel="noreferrer" >Project: Pixel Art</a>
+        <a href="https://armando771.github.io/Project-Shopping-Card/" target="_blank" rel="noreferrer">Project: Shopping Cart</a>
+        <a href="https://github.com/tryber/sd-012-project-zoo-functions/blob/Armando-Guerra-zoo-functions-project/src/zoo.js" target="_blank" rel="noreferrer">Project: Zoo Functions</a>
+        <a href="https://github.com/tryber/sd-012-project-jest/tree/armando-guerra-project-jest/test" target="_blank" rel="noreferrer">Project: Test Jest</a>
+        <a href="https://github.com/tryber/sd-012-project-react-testing-library/tree/Armando-Guerra-Project-React-Testing-Library/src/tests" target="_blank" rel="noreferrer">Project: Test React Library</a>
+        <a href="https://github.com/tryber/sd-12-project-frontend-online-store/tree/main-group-7/src/components" target="_blank" rel="noreferrer">Project: Front End Online Store</a>
+        <a href="https://armando771.github.io/Project-Todo-List/" target="_blank" rel="noreferrer">Project: Todo List</a>
         </div>
       </div>
     );
   };
-}
 
 export default Projects;
