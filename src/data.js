faddd3f9445dc1cc781391247c2f20a15b041a19
Armando <a.guerra58@hotmail.com>
2021-10-07 16:37:37 -0300

    changes

--- src/data.js
+++ src/data.js
@@ -1,77 +1,92 @@
-const FrontEndInformations= [
-
+const FrontEndInformations = [
   {
-    URLS:"https://armando771.github.io/Color-Palet.github.io/",
-    ProjectName:"Project Color-Palet",
-    AboutProject: "pixel art project that allows the user to color, erase, change small squares to the desired color"
+    URLS: "https://armando771.github.io/ProjectTriviaRedux/",
+    ProjectName: "Trivia Game Redux",
+    AboutProject:
+      "Project made using React.js and Redux where we used a Trivia API to create a mini quiz game, with ranking score",
+  },
+  {
+    URLS: "https://armando771.github.io/Project-FrontEnd/",
+    ProjectName: "Front End Online Store",
+    AboutProject:
+      "Project in React.JS made using an API from Mercado Livre. Where we interact with products and categories to make a functionality interface similar to the original site",
   },
   {
     URLS: "https://armando771.github.io/Project-Shopping-Card/",
     ProjectName: "Project Shopping-Card",
-    AboutProject: "Project in React.JS made using an API from Mercado Livre. Where we interact with products and categories to make a functionality interface similar to the original site"
-  },
-  {  URLS: "https://armando771.github.io/Project-Todo-List/",
-     ProjectName: "Project TodoList",
-     AboutProject: "Task List Project. Where it allows the user to add, remove, mark as completed, everyday tasks"
+    AboutProject:
+      "Project made using an API from Mercado Livre. Where we interact with products to make a functionality interface similar to the original site",
   },
   {
     URLS: "https://armando771.github.io/ProjectMovieCrud/",
     ProjectName: "Project Movie-Crud",
-    AboutProject: "Project in React.JS made using API simulations. Using the CRUD concept {Create, Read, Update, Delete}. Using dynamic routes with react router dom"
+    AboutProject:
+      "Project in React.JS made using API simulations. Using the CRUD concept {Create, Read, Update, Delete}. Using dynamic routes with react router dom",
   },
   {
     URLS: "https://armando771.github.io/Project-Movie-Library/",
     ProjectName: "Project Movie-Library",
-    AboutProject: "Project in React.JS using componentization logic to make renderings "
-  }
-]
-
+    AboutProject:
+      "Project in React.JS using componentization logic to make renderings ",
+  },
+  {
+    URLS: "https://armando771.github.io/Project-Todo-List/",
+    ProjectName: "Project TodoList",
+    AboutProject:
+      "Task List Project. Where it allows the user to add, remove, mark as completed, everyday tasks",
+  },
+  {
+    URLS: "https://armando771.github.io/Color-Palet.github.io/",
+    ProjectName: "Project Color-Palet",
+    AboutProject:
+      "pixel art project that allows the user to color, erase, change small squares to the desired color",
+  },
+];
 
 const FuncionsInformations = [
   {
     URLS: "",
     ProjectName: "",
-    AboutProject: ""
+    AboutProject: "",
   },
   {
     URLS: "",
     ProjectName: "",
-    AboutProject: ""
+    AboutProject: "",
   },
   {
     URLS: "",
     ProjectName: "",
-    AboutProject: ""
+    AboutProject: "",
   },
   {
     URLS: "",
     ProjectName: "",
-    AboutProject: ""
+    AboutProject: "",
   },
   {
     URLS: "",
     ProjectName: "",
-    AboutProject: ""
+    AboutProject: "",
   },
-]
+];
 
 const BackEndInformations = [
   {
     URLS: "",
     ProjectName: "",
-    AboutProject: ""
+    AboutProject: "",
   },
   {
     URLS: "",
     ProjectName: "",
-    AboutProject: ""
+    AboutProject: "",
   },
   {
     URLS: "",
     ProjectName: "",
-    AboutProject: ""
+    AboutProject: "",
   },
-]
+];
 
-
-export {FuncionsInformations, FrontEndInformations, BackEndInformations};
+export { FuncionsInformations, FrontEndInformations, BackEndInformations };
